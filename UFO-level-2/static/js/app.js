b42e0a317dab4a0f21b856ffb3923bf9be1b38d2
dylanm2094 <59807851+dylanm2094@users.noreply.github.com>
2020-05-18 00:56:54 -0400

    adding conditionals

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -30,12 +30,18 @@ function runEnter() {
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-  var shapeOption = d3.select("#exampleFormControlSelect1");
+  var shapeOption = d3.select("#shapes");
+  var cityOption = d3.select("#cities");
+  var stateOption = d3.select("#states");
+  var countryOption = d3.select("#countries");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
   var shapeValue = shapeOption.property("value");
-  console.log(shapeValue)
+  var cityValue = cityOption.property("value");
+  var stateValue = stateOption.property("value");
+  var countryValue = countryOption.property("value");
+  
 
 
   var table = document.getElementById("ufo-table");
@@ -43,14 +49,93 @@ function runEnter() {
   for (i = 1; i <= data.length; i++) {
     var tr = table.rows[i];
     var date = tr.cells[0].textContent;
+    var city = tr.cells[1].textContent;
+    var state = tr.cells[2].textContent;
+    var country = tr.cells[3].textContent;
     var shape = tr.cells[4].textContent;
-    if (inputValue === "" & shapeValue === "--Select--") {
+    if (inputValue === "" & shapeValue === "--Select--" 
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
       tr.style.display = "";
-    } else if (date === inputValue & shapeValue === "--Select--") {
+    } else if (inputValue === date & shapeValue === "--Select--" 
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
       tr.style.display = "";
-    } else if (date === inputValue & shape === shapeValue) {
+    } else if (inputValue === "" & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
       tr.style.display = "";
-    } else if (inputValue === "" & shape === shapeValue) {
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === city & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === "--Select--" & stateValue === state 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === "--Select--"
+    & cityValue === city & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === "--Select--"
+    & cityValue === "--Select--" & stateValue === state 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === "--Select--"
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === shape
+    & cityValue === city & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === state 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === city & stateValue === state 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === city & stateValue === "--Select--" 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === "" & shapeValue === "--Select--"
+    & cityValue === "--Select--" & stateValue === state 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === shape
+    & cityValue === city & stateValue === "--Select--" 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === state 
+    & countryValue === "--Select--") {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === shape
+    & cityValue === "--Select--" & stateValue === "--Select--" 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === "--Select--"
+    & cityValue === city & stateValue === state 
+    & countryValue === country) {
+      tr.style.display = "";
+    } else if (inputValue === date & shapeValue === shape
+    & cityValue === city & stateValue === state 
+    & countryValue === country) {
       tr.style.display = "";
     } else {
       tr.style.display = "none";
@@ -60,7 +145,13 @@ function runEnter() {
 
 
 var uniqueShapes = []
-var sel = document.getElementById('exampleFormControlSelect1');
+var uniqueCities = []
+var uniqueStates = []
+var uniqueCountries = []
+var sel_shps = document.getElementById('shapes');
+var sel_city = document.getElementById('cities');
+var sel_state = document.getElementById('states');
+var sel_ctry = document.getElementById('countries');
 
 // create new option element
 var opt = document.createElement('option');
@@ -71,7 +162,28 @@ data.forEach(ufoSighting => {
     var opt = document.createElement('option');
     opt.appendChild(document.createTextNode(ufoSighting.shape));
     opt.value = ufoSighting.shape;
-    sel.appendChild(opt); 
+    sel_shps.appendChild(opt); 
+  }
+  if(uniqueCities.indexOf(ufoSighting.city) === -1) {
+    uniqueCities.push(ufoSighting.city);
+    var opt = document.createElement('option');
+    opt.appendChild(document.createTextNode(ufoSighting.city));
+    opt.value = ufoSighting.city;
+    sel_city.appendChild(opt); 
+  }
+  if(uniqueStates.indexOf(ufoSighting.state) === -1) {
+    uniqueStates.push(ufoSighting.state);
+    var opt = document.createElement('option');
+    opt.appendChild(document.createTextNode(ufoSighting.state));
+    opt.value = ufoSighting.state;
+    sel_state.appendChild(opt); 
+  }
+  if(uniqueCountries.indexOf(ufoSighting.country) === -1) {
+    uniqueCountries.push(ufoSighting.country);
+    var opt = document.createElement('option');
+    opt.appendChild(document.createTextNode(ufoSighting.country));
+    opt.value = ufoSighting.country;
+    sel_ctry.appendChild(opt); 
   }
 });
 
@@ -79,5 +191,5 @@ var reset = d3.select("#reset-btn");
 
 reset.on("click", function() {
   document.getElementById("my-form").reset();
-  runEnter()
+  runEnter();
 });
