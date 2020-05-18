f2ed5653fee5e06a769cb8a321593bf271064c54
dylanm2094 <59807851+dylanm2094@users.noreply.github.com>
2020-05-17 23:08:41 -0400

    finished level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,8 +12,6 @@ data.forEach(ufoSighting => {
     row.append("td").text(ufoSighting.comments);
 });
 
-var tr = d3.select("tr");
-
 // Select the button
 var button = d3.select("#filter-btn");
 
@@ -30,24 +28,25 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  tr.remove();
-
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  var filteredData = data.filter(ufoSighting => ufoSighting.datetime === inputValue);
+  var table = document.getElementById("ufo-table");
 
-  filteredData.forEach(ufoSighting => {
-    var row = tbody.append("tr");
-    row.append("td").text(ufoSighting.datetime);
-    row.append("td").text(ufoSighting.city);
-    row.append("td").text(ufoSighting.state);
-    row.append("td").text(ufoSighting.country);
-    row.append("td").text(ufoSighting.shape);
-    row.append("td").text(ufoSighting.durationMinutes);
-    row.append("td").text(ufoSighting.comments);
-    });
+  for (i = 1; i <= data.length; i++) {
+    var tr = table.rows[i];
+    var value = tr.cells[0].textContent;
+    if (inputValue === "") {
+      tr.style.display = "";
+    } else if (value === inputValue) {
+      tr.style.display = "";
+    } else {
+      tr.style.display = "none";
+    }
+  }
+
+  //var filteredData = data.filter(ufoSighting => ufoSighting.datetime === inputValue);
 };
